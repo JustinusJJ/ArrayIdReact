1d1c321260609aa7854256086e6828b46172af70
Justinus J J <john.justinus@gmail.com>
2022-01-26 13:53:44 +0700

    Event handling

--- public/app.js
+++ public/app.js
@@ -1,6 +1,19 @@
 const root = document.querySelector('#root');
-const className = 'box';
-const element = /*#__PURE__*/React.createElement("div", {
-  className: className
-});
+
+function clickMe(msg) {
+  alert(msg);
+} // // Cara 1
+// const element = (
+//   <button onClick={function () {
+//     clickMe('Hello');
+//   }}>
+//     Click Here
+//   </button>
+// );
+// Cara 2
+
+
+const element = /*#__PURE__*/React.createElement("button", {
+  onClick: clickMe.bind(this, 'Hello Again')
+}, "Click Here");
 ReactDOM.render(element, root); // Render element
--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const root = document.querySelector('#root');
 
-const className = 'box';
-const element = (
-  <div className={className}></div>
-);
+function clickMe(msg) {
+  alert(msg);
+}
+// // Cara 1
+// const element = (
+//   <button onClick={function () {
+//     clickMe('Hello');
+//   }}>
+//     Click Here
+//   </button>
+// );
   
+// Cara 2
+const element = (
+  <button onClick={clickMe.bind(this, 'Hello Again')}>
+    Click Here
+  </button>
+);
+
 ReactDOM.render(element, root); // Render element
