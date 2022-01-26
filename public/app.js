39a151e1ebef8589e6b739162be03fe1cc2ec836
Justinus J J <john.justinus@gmail.com>
2022-01-26 12:53:20 +0700

    Basic style React element (inline style)

--- public/app.js
+++ public/app.js
@@ -1,21 +1,9 @@
 const root = document.querySelector('#root');
-console.log("Auto");
-
-function tick() {
-  //  React updates only the value
-  const element = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Time Now"), /*#__PURE__*/React.createElement("h2", null, new Date().toLocaleTimeString()));
-  ReactDOM.render(element, root); // Render element
-  // // Js Updates the whole div
-  // const element = `
-  //   <div>
-  //     <h1>Time Now</h1>
-  //     ${new Date().toLocaleTimeString()}
-  //   </div>
-  // `;
-  // root.innerHTML = element;
-}
-
-tick();
-setInterval(function () {
-  tick();
-}, 1000);
+const element = /*#__PURE__*/React.createElement("div", {
+  style: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'blue'
+  }
+});
+ReactDOM.render(element, root); // Render element
--- src/app.js
+++ src/app.js
@@ -1,29 +1,11 @@
 const root = document.querySelector('#root');
 
-console.log("Auto Compile");
-
-function tick() {
-  //  React updates only the value
-  const element = (
-    <div>
-      <h1>Time Now</h1>
-      <h2>{new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
+const element = (
+  <div style={{
+    width: 200,
+    height: 200,
+    backgroundColor: 'blue',
+  }}></div>
+);
   
-  ReactDOM.render(element, root); // Render element
-  // // Js Updates the whole div
-  // const element = `
-  //   <div>
-  //     <h1>Time Now</h1>
-  //     ${new Date().toLocaleTimeString()}
-  //   </div>
-  // `;
-
-  // root.innerHTML = element;
-}
-
-tick();
-setInterval(function () {
-  tick();
-}, 1000);
+ReactDOM.render(element, root); // Render element
