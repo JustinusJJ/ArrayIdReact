19c0ffc4737c9fd56ce7212b6e1b5b10cd044709
Justinus J J <john.justinus@gmail.com>
2022-01-27 13:06:39 +0700

    Styling in CRA

--- tutorial-react-cra/src/App.js
+++ tutorial-react-cra/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import { WelcomeH2 } from './Welcome.js'
+import Button from './Button.js'
 
 function App() {
   return (
     <div className="App">
-      <WelcomeH2 children="Welcome 1" />
-      <WelcomeH2 children="Welcome 2" />
-      <WelcomeH2>
-        This is a Content
-      </WelcomeH2>
+      <Button>Click Me</Button>
     </div>
   );
 }
