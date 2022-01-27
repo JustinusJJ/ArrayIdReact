17511f14725f998b56557761257f5f2fe00267bf
Justinus J J <john.justinus@gmail.com>
2022-01-27 14:01:38 +0700

    Basic Route

--- tutorial-react-cra/src/App.js
+++ tutorial-react-cra/src/App.js
@@ -1,9 +1,19 @@
+import { Routes, Route, Link } from 'react-router-dom' 
 import './App.css';
+import About from './About';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Halaman Home</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
