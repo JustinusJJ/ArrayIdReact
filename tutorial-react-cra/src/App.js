0ade7287df631989672ccd44aee2235d0291bcbd
Justinus J J <john.justinus@gmail.com>
2022-01-27 12:52:18 +0700

    Remove ESLint Warning

--- tutorial-react-cra/src/App.js
+++ tutorial-react-cra/src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
-// import Welcome, { WelcomeH2, WelcomeH3 } from './Welcome.js'
 import { WelcomeH2, WelcomeH3 } from './Welcome.js'
 
 function App() {
   return (
     <div className="App">
-      {/* <Welcome /> */}
       <WelcomeH2 />
       <WelcomeH3 />
     </div>
