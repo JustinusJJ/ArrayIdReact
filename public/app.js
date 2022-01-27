a75ae648f64c4a38ebceb9d97597e3b7a252e20c
Justinus J J <john.justinus@gmail.com>
2022-01-27 10:35:43 +0700

    Todo App

--- public/app.js
+++ public/app.js
@@ -1,39 +1,122 @@
 const root = document.querySelector('#root');
 
 function App() {
-  const [news, setNews] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
-  React.useEffect(function () {
-    // Cara 1
-    // const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs')
-    //   .then(function(response) {
-    //     // console.log(response);
-    //     // console.log(response.json());
-    //     return response.json();
-    //     //response.JSON -> async
-    //   }).then(function (response) {
-    //     console.log(response);
-    //   }
-    // );
-    // // console.log(getData);
-    // // Promise -> Async, background process, you can do other stuff, no need to wait fetch
-    // Cara 2
-    async function getData() {
-      const request = await fetch('https://api.spaceflightnewsapi.net/v3/blogs'); // console.log(request);
+  const [activity, setActivity] = React.useState('');
+  const [edit, setEdit] = React.useState({});
+  const [todos, setTodos] = React.useState([]);
+  const [message, setMessage] = React.useState('');
 
-      const response = await request.json();
-      setNews(response);
-      setLoading(false);
+  function generateId() {
+    return Date.now();
+  }
+
+  function saveTodoHandler(event) {
+    event.preventDefault();
+
+    if (!activity) {
+      // return console.log('Insert');
+      return setMessage('Activity can\'t be nil');
     }
 
-    getData();
-  }, []);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Data Fetch"), loading ? /*#__PURE__*/React.createElement("i", null, "Loading data ...") : /*#__PURE__*/React.createElement("ul", null, news.map(function (item) {
-    // console.log(item);
+    setMessage('');
+
+    if (edit.id) {
+      // console.log('edit');
+      const updatedTodo = { ...edit,
+        activity
+      }; // console.log(updatedTodo);
+
+      const editTodoIndex = todos.findIndex(function (todo) {
+        return todo.id == edit.id;
+      });
+      const updatedTodos = [...todos]; // console.log(editTodoIndex);
+      // console.log(updatedTodos[editTodoIndex]);
+
+      updatedTodos[editTodoIndex] = updatedTodo; // console.log(updatedTodos);
+
+      setTodos(updatedTodos);
+      return cancelEditHandler();
+    }
+
+    setTodos([...todos, {
+      id: generateId(),
+      // activity: activity
+      activity,
+      done: false
+    }]);
+    setActivity('');
+  }
+
+  function removeTodoHandler(todoId) {
+    // console.log(todoId);
+    const filteredTodos = todos.filter(function (todo) {
+      // console.log(todoId, todo.id);
+      return todo.id != todoId;
+    }); // console.log(filteredTodos);
+    // console.log(todos);
+
+    setTodos(filteredTodos);
+    if (edit.id) cancelEditHandler();
+  }
+
+  function editTodoHandler(todo) {
+    setActivity(todo.activity);
+    setEdit(todo);
+  }
+
+  function cancelEditHandler() {
+    // console.log('Cancel Edit');
+    setEdit({});
+    setActivity('');
+  }
+
+  function doneTodoHandler(todo) {
+    // console.log(todo);
+    const updatedTodo = { // id: todo.id,
+      // activity: todo.activity,
+      ...todo,
+      done: todo.done ? false : true
+    }; // console.log(updatedTodo);
+
+    const editTodoIndex = todos.findIndex(function (currentTodo) {
+      return currentTodo.id == todo.id;
+    });
+    const updatedTodos = [...todos];
+    updatedTodos[editTodoIndex] = updatedTodo; // console.log(updatedTodos);
+
+    setTodos(updatedTodos);
+  }
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Simple To-do List"), message && /*#__PURE__*/React.createElement("div", {
+    style: {
+      color: 'red'
+    }
+  }, message), /*#__PURE__*/React.createElement("form", {
+    onSubmit: saveTodoHandler
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    placeholder: "Activity Name",
+    value: activity,
+    onChange: function (event) {
+      setActivity(event.target.value);
+    }
+  }), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, edit.id ? 'Change' : 'Add'), edit.id && /*#__PURE__*/React.createElement("button", {
+    onClick: cancelEditHandler
+  }, "Cancel Edit")), todos.length > 0 ? /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
     return /*#__PURE__*/React.createElement("li", {
-      key: item.id
-    }, item.title);
-  })));
+      key: todo.id
+    }, /*#__PURE__*/React.createElement("input", {
+      type: "checkbox",
+      checked: todo.done,
+      onChange: doneTodoHandler.bind(this, todo)
+    }), todo.activity, "(", todo.done ? 'DONE' : 'Unfinished', ")", /*#__PURE__*/React.createElement("button", {
+      onClick: editTodoHandler.bind(this, todo)
+    }, "Edit"), /*#__PURE__*/React.createElement("button", {
+      onClick: removeTodoHandler.bind(this, todo.id)
+    }, "Hapus"));
+  })) : /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("i", null, "Empty")));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,55 +1,159 @@
 const root = document.querySelector('#root');
 
 function App() {
-  const [news, setNews] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
-
-  React.useEffect(function () {
-    // Cara 1
-    // const getData = fetch('https://api.spaceflightnewsapi.net/v3/blogs')
-    //   .then(function(response) {
-    //     // console.log(response);
-    //     // console.log(response.json());
-    //     return response.json();
-    //     //response.JSON -> async
-    //   }).then(function (response) {
-    //     console.log(response);
-    //   }
-    // );
-    // // console.log(getData);
-    // // Promise -> Async, background process, you can do other stuff, no need to wait fetch
+  const [activity, setActivity] = React.useState('');
+  const [edit, setEdit] = React.useState({});
+  const [todos, setTodos] = React.useState([]);
+  const [message, setMessage] = React.useState('');
   
-    // Cara 2
-    async function getData() {
-      const request = await fetch(
-        'https://api.spaceflightnewsapi.net/v3/blogs'
-      );
-      // console.log(request);
+  function generateId() {
+    return Date.now();
+  }
 
-      const response = await request.json();
+  function saveTodoHandler(event) {
+    event.preventDefault();
 
-      setNews(response);
-      setLoading(false);
+    if(!activity) {
+      // return console.log('Insert');
+      return setMessage('Activity can\'t be nil');
     }
-    getData();
-  }, []);
+
+    setMessage('');
+
+    if (edit.id) {
+      // console.log('edit');
+      const updatedTodo = {
+        ... edit,
+        activity
+      };
+      // console.log(updatedTodo);
+      const editTodoIndex = todos.findIndex(function (todo) {
+        return todo.id == edit.id;
+      });
+
+      const updatedTodos = [
+        ... todos
+      ];
+
+      // console.log(editTodoIndex);
+      // console.log(updatedTodos[editTodoIndex]);
+      updatedTodos[editTodoIndex] = updatedTodo;
+      // console.log(updatedTodos);
+
+      
+      setTodos(updatedTodos);
+
+      return cancelEditHandler();
+    }
+
+    setTodos([
+      ... todos, {
+        id: generateId(),
+        // activity: activity
+        activity,
+        done: false
+      }
+    ]);
+
+    setActivity('');
+  }
+
+  function removeTodoHandler(todoId) {
+    // console.log(todoId);
+    const filteredTodos = todos.filter(function (todo) {
+      // console.log(todoId, todo.id);
+      return todo.id != todoId;
+    });
+
+    // console.log(filteredTodos);
+    // console.log(todos);
+    setTodos(filteredTodos);
+    if(edit .id) cancelEditHandler();
+  }
+
+  function editTodoHandler(todo) {
+    setActivity(todo.activity);
+    setEdit(todo);
+  }
+
+  function cancelEditHandler() {
+    // console.log('Cancel Edit');
+    setEdit({});
+    setActivity('');
+  }
+
+  function doneTodoHandler(todo) {
+    // console.log(todo);
+    const updatedTodo = {
+      // id: todo.id,
+      // activity: todo.activity,
+      ... todo,
+      done: todo.done ? false : true
+    };
+    
+    // console.log(updatedTodo);
+
+    const editTodoIndex = todos.findIndex(function (currentTodo) {
+      return currentTodo.id == todo.id;
+    });
+
+    const updatedTodos = [ ... todos];
+    updatedTodos[editTodoIndex] = updatedTodo
+
+    // console.log(updatedTodos);
+    setTodos(updatedTodos);
+  }
 
   return (
     <>
-      <h1>Data Fetch</h1>
-      {loading ? (
-        <i>Loading data ...</i>
-        ) : (
-          <ul>
-            {news.map(function (item) {
-              // console.log(item);
-              return <li key={item.id}>{item.title}</li>
-            })}
-          </ul>
-        )
-      }
+      <h1>Simple To-do List</h1>
+      {message && <div style={{color: 'red'}}>
+        {message}
+      </div>}
+      <form onSubmit={saveTodoHandler}>
+        <input
+          type="text"
+          placeholder="Activity Name"
+          value={activity}
+          onChange={function (event) {
+            setActivity(event.target.value);
+          }}
+        />
+        <button type="submit">
+          {edit.id ? 'Change' : 'Add'}
+        </button>
+        {edit.id && <button onClick={cancelEditHandler}>
+            Cancel Edit
+          </button>}
+      </form>
+      {todos.length > 0 ? (
+        <ul>
+          {todos.map(function (todo) {
+            return (
+              <li key={todo.id}>
+                <input
+                  type="checkbox"
+                  checked={todo.done}
+                  onChange={doneTodoHandler.bind(this, todo)} />
+                {todo.activity}
+                ({todo.done ? 'DONE' : 'Unfinished'})
+                <button onClick={editTodoHandler.bind(this, todo)}>
+                  Edit
+                </button>
+                <button onClick={removeTodoHandler.bind(this, todo.id)}>
+                  Hapus
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>
+          <i>Empty</i>
+        </p>
+      )}
     </>
-  );
+  )
 }
 
 ReactDOM.render(<App />, root);
